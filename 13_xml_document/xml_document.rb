82a050ecdfc6cdca2a144e2e915c62f157551b5f
Daneil Skrybajlo <skrybajlo.d.yu@gmail.com>
2018-07-16 20:51:27 +0300

    все одно піндус стайл

--- 13_xml_document/xml_document.rb
+++ 13_xml_document/xml_document.rb
@@ -5,38 +5,40 @@ class XmlDocument
     @level = 0
   end
 
-  def method_missing(symbol, *args, &block)
-    @level+=1
+  def method_missing(symbol, *args)
+    @level += 1
     values ||= {}
     name = symbol.id2name
-    unless args.empty?
-      values[name] = args[0]
-    else
-      values[name] = (block_given?)? yield : nil
-    end
+    values[name] = if args.empty?
+                     block_given? ? yield : nil
+                   else
+                     args[0]
+                   end
 
-    r= hash2str(values)
-    @level = @level.pred
-    r
+    result = hash2str(values)
+    @level -= 1
+    result
   end
 
   private
 
   def hash2str(hash)
-
     e = "\n" if @set_enters
     b = ''
     b = '  ' if @set_enters
-    hash.each_pair do |k, v|
-      return "#{b*(@level-1)}<#{k}/>#{e}" unless v
-      if v.class == Hash
-        v.each_pair do |k1, v1|
-          return "#{b*(@level-1)}<#{k} #{k1}='#{v1}'/>#{e}"
+    hash.each_pair do |key, val|
+      return "#{b * (@level - 1)}<#{key}/>#{e}" unless val
+      if val.class == Hash
+        val.each_pair do |sub_key, sub_val|
+          return "#{b * (@level - 1)}<#{key} "\
+                    "#{sub_key}='#{sub_val}'/>"\
+                    "#{e}"
         end
       else
-        return "#{b*(@level-1)}<#{k}>#{e}#{v}#{b*(@level-1)}</#{k}>#{e}"
+        return "#{b * (@level - 1)}<#{key}>#{e}"\
+                    "#{val}"\
+                "#{b * (@level - 1)}</#{key}>#{e}"
       end
     end
   end
-
 end
